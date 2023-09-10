abb058900e3022bea183078716ad0d3c1bc4e2f1
Okokrko <gabrielamankwaa617@gmail.com>
2023-09-10 18:41:11 +0000

    Variables If else

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * main - Entry point
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	fwrite(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
