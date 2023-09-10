1766772737a56b52f258efe769faa9804d59e624
Okokrko <gabrielamankwaa617@gmail.com>
2023-09-10 18:17:55 +0000

    Variables If else

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -10,14 +10,14 @@
 
 int main(void)
 {
-        char alphabet = 'z';
+	char alphabet = 'z';
 
-        while (alphabet <= 'a')
-        {
-                putchar(alphabet);
-                alphabet++;
-        }
-        putchar('\n');
-
-        return (0);
+	while (alphabet <= 'a')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar('\n');
+	
+	return (0);
 }
